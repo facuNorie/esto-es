e43d0537b4d45601cfbb14d444efa31548bf9d83
Facu Noriega <noriegafacundo08@gmail.com>
2022-05-24 17:51:00 -0300

    Showing the projects

--- src/components/project_item/index.jsx
+++ src/components/project_item/index.jsx
@@ -1,14 +1,57 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, IconButton, Popover, Button } from '@mui/material'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
 import React from 'react'
 
-export default function ProjectItem() {
+export default function ProjectItem({ item }) {
+  console.log(item)
+  const [showOptions, setShowOptions] = React.useState(false)
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
   return (
-    <Box display='flex' justifyContent='space-around'>
-      <Typography>Landing page</Typography>
-      <Typography>Walt consani</Typography>
-      <Typography>Igna truffa</Typography>
-      <Typography>Enabled</Typography>
-      <Typography>...</Typography>
+    <Box
+      display='flex'
+      justifyContent='space-around'
+      alignItems='center'
+      borderBottom='1px solid gray'
+      p={1}
+    >
+      <div>
+        <Typography>{item.projectName}</Typography>
+        <Typography sx={{ color: 'text.secondary' }} variant='subtitle2'>
+          {item.date.toLocaleString()}
+        </Typography>
+      </div>
+
+      <Typography>{item.projectManager}</Typography>
+      <Typography>{item.assignedTo} </Typography>
+      <Typography>{item.enabled}</Typography>
+      <IconButton onClick={(event) => setAnchorEl(event.currentTarget)}>
+        <MoreVertIcon />
+      </IconButton>
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'right',
+        }}
+      >
+        <Box display='flex' flexDirection='column'>
+          <Button sx={{ color: 'text.secondary' }} startIcon={<EditIcon />}>
+            Edit
+          </Button>
+          <Button sx={{ color: 'text.secondary' }} startIcon={<DeleteIcon />}>
+            Delete
+          </Button>
+        </Box>
+      </Popover>
     </Box>
   )
 }
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,17 +1,13 @@
 import { Box, Divider, Typography } from '@mui/material'
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/context'
 import ProjectItem from '../project_item'
 
-let number = [1, 2, 3, 4, 5]
 export default function Projects() {
+  const { projects } = useContext(AppContext)
+
   return (
-    <Box
-      sx={{
-        backgroundColor: '#F0F2F5',
-        height: '100vh',
-        padding: '32px',
-      }}
-    >
+    <Box>
       <Box sx={{ boxShadow: 1 }}>
         <Box
           display='flex'
@@ -26,14 +22,9 @@ export default function Projects() {
           <Typography>Action</Typography>
         </Box>
         <Divider />
-        <Box
-          sx={{ bgcolor: 'white' }}
-          display='flex'
-          flexDirection='column'
-          p={1}
-        >
-          {number.map((item) => {
-            return <ProjectItem key={item} />
+        <Box sx={{ bgcolor: 'white' }} display='flex' flexDirection='column'>
+          {projects.map((item) => {
+            return <ProjectItem key={item.id} item={item} />
           })}
         </Box>
       </Box>
