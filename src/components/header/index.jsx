eb223dd63f7fc1a22224b481eea3053e7a5cf37b
Facu Noriega <noriegafacundo08@gmail.com>
2022-05-24 21:36:54 -0300

    making the buttons reactive

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,21 +1,32 @@
 import { Box, Button, Typography } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import { useContext } from 'react'
 import { AppContext } from '../../context/context'
 
 export default function Header() {
-  const { setShowSection, showSection } = useContext(AppContext)
+  const { setShowSection, showSection, setEditionMode, editionMode } =
+    useContext(AppContext)
   return (
     <Box display='flex' justifyContent='space-between' p={1}>
-      <Typography>My projects</Typography>
+      <Typography variant='h6'>
+        {showSection
+          ? 'My Projects'
+          : editionMode
+          ? 'Edit project'
+          : 'Add project'}
+      </Typography>
       <Button
         size='small'
         color='error'
         variant='contained'
-        startIcon={<AddIcon />}
-        onClick={() => setShowSection(!showSection)}
+        startIcon={showSection ? <AddIcon /> : <ArrowBackIcon />}
+        onClick={() => {
+          setShowSection(!showSection)
+          setEditionMode(false)
+        }}
       >
-        Add project
+        {showSection ? 'Add project' : 'Back'}
       </Button>
     </Box>
   )
