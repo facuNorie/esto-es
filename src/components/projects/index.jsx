ae98c0460d340ed19939a3d98a32d82fbc58cbf4
Facu Noriega <noriegafacundo08@gmail.com>
2022-05-24 22:29:36 -0300

    Adding filter componet

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,13 +1,36 @@
-import { Box, Container, Divider, Grid, Typography } from '@mui/material'
+import { Box, TextField, Divider, Grid, Typography } from '@mui/material'
 import React, { useContext } from 'react'
 import { AppContext } from '../../context/context'
 import ProjectItem from '../project_item'
 
+import InputAdornment from '@mui/material/InputAdornment'
+import SearchIcon from '@mui/icons-material/Search'
+
 export default function Projects() {
   const { projects } = useContext(AppContext)
-
+  const [inputFilter, setInputFilter] = React.useState('')
+  const handleInput = (e) => {
+    setInputFilter(e.target.value)
+  }
   return (
     <Box>
+      <Box sx={{ marginBottom: '10px' }} px={1}>
+        <TextField
+          fullWidth
+          color='error'
+          type='text'
+          label='search...'
+          onChange={handleInput}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
       <Box sx={{ boxShadow: 1 }}>
         <Box component='span' sx={{ display: { xs: 'none', md: 'block' } }}>
           <Grid container p={2} sx={{ bgcolor: 'grey.50' }}>
@@ -31,7 +54,12 @@ export default function Projects() {
         <Divider />
         <Box sx={{ bgcolor: 'white' }} display='flex' flexDirection='column'>
           {projects.map((item) => {
-            return <ProjectItem key={item.id} item={item} />
+            let toCompare = item?.projectName?.toLowerCase()
+            if (inputFilter.length === 0)
+              return <ProjectItem key={item.id} item={item} />
+            else if (toCompare.includes(inputFilter.toLowerCase())) {
+              return <ProjectItem key={item.id} item={item} />
+            } else return <span></span>
           })}
         </Box>
       </Box>
