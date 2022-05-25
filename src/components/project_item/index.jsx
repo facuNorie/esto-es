1bd5d12bb02028fce8232ba9ec19e54d195f0632
Facu Noriega <noriegafacundo08@gmail.com>
2022-05-24 21:38:02 -0300

    Using grid system

--- src/components/project_item/index.jsx
+++ src/components/project_item/index.jsx
@@ -1,57 +1,107 @@
-import { Box, Typography, IconButton, Popover, Button } from '@mui/material'
+import {
+  Box,
+  Typography,
+  IconButton,
+  Popover,
+  Button,
+  Avatar,
+  Chip,
+  Grid,
+  Modal,
+} from '@mui/material'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import EditIcon from '@mui/icons-material/Edit'
 import DeleteIcon from '@mui/icons-material/Delete'
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/context'
 
 export default function ProjectItem({ item }) {
-  console.log(item)
-  const [showOptions, setShowOptions] = React.useState(false)
+  const { deleteProject, setToEdit } = useContext(AppContext)
   const [anchorEl, setAnchorEl] = React.useState(null)
 
+  const handleDelete = () => {
+    let response = confirm('Are you sure you want to delete this project?')
+    if (response) deleteProject(item.id)
+    else setAnchorEl(null)
+  }
+
   return (
-    <Box
-      display='flex'
-      justifyContent='space-around'
-      alignItems='center'
-      borderBottom='1px solid gray'
-      p={1}
-    >
-      <div>
+    <Grid container borderBottom='1px solid gray' p={2}>
+      <Grid item xs={10} md={3}>
         <Typography>{item.projectName}</Typography>
         <Typography sx={{ color: 'text.secondary' }} variant='subtitle2'>
-          {item.date.toLocaleString()}
+          Creation date: {item.date.toLocaleString()}
         </Typography>
-      </div>
+      </Grid>
 
-      <Typography>{item.projectManager}</Typography>
-      <Typography>{item.assignedTo} </Typography>
-      <Typography>{item.enabled}</Typography>
-      <IconButton onClick={(event) => setAnchorEl(event.currentTarget)}>
-        <MoreVertIcon />
-      </IconButton>
-      <Popover
-        open={Boolean(anchorEl)}
-        anchorEl={anchorEl}
-        onClose={() => setAnchorEl(null)}
-        anchorOrigin={{
-          vertical: 'center',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'center',
-          horizontal: 'right',
-        }}
+      <Grid
+        item
+        sx={{ display: { xs: 'none', md: 'flex' } }}
+        alignItems='center'
+        xs={3}
       >
-        <Box display='flex' flexDirection='column'>
-          <Button sx={{ color: 'text.secondary' }} startIcon={<EditIcon />}>
-            Edit
-          </Button>
-          <Button sx={{ color: 'text.secondary' }} startIcon={<DeleteIcon />}>
-            Delete
-          </Button>
-        </Box>
-      </Popover>
-    </Box>
+        <Avatar
+          sx={{ width: 24, height: 24, fontSize: '1rem', marginRight: '8px' }}
+        >
+          {item.projectManager.split('', 1)}
+        </Avatar>
+        <Typography>{item.projectManager}</Typography>
+      </Grid>
+      <Grid
+        item
+        display='flex'
+        alignItems='center'
+        textAlign='center'
+        xs={10}
+        md={3}
+      >
+        <Avatar
+          sx={{ width: 24, height: 24, fontSize: '1rem', marginRight: '8px' }}
+        >
+          {item.assignedTo.split('', 1)}
+        </Avatar>
+        <Typography>{item.assignedTo} </Typography>
+      </Grid>
+      <Grid item xs={2} sx={{ display: { xs: 'none', md: 'inline' } }}>
+        <Chip label={item.enabled}></Chip>
+      </Grid>
+      <Grid item xs={2} md={1}>
+        <IconButton onClick={(event) => setAnchorEl(event.currentTarget)}>
+          <MoreVertIcon />
+        </IconButton>
+        <Popover
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          onClose={() => setAnchorEl(null)}
+          anchorOrigin={{
+            vertical: 'center',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'center',
+            horizontal: 'right',
+          }}
+        >
+          <Box display='flex' flexDirection='column'>
+            <Button
+              sx={{ color: 'text.secondary' }}
+              startIcon={<EditIcon />}
+              onClick={() => {
+                setToEdit(item.id)
+              }}
+            >
+              Edit
+            </Button>
+            <Button
+              sx={{ color: 'text.secondary' }}
+              startIcon={<DeleteIcon />}
+              onClick={handleDelete}
+            >
+              Delete
+            </Button>
+          </Box>
+        </Popover>
+      </Grid>
+    </Grid>
   )
 }
